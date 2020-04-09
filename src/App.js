4e67ae6a8da455aa61aef792d63794767a9280cc
Carlos Junior <carlosjunior1983@gmail.com>
2020-04-09 16:57:57 -0300

    Projeto simpes de Calculadora de Gorjetas em React.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { useState } from 'react';
+import styled from 'styled-components' ;
 
-function App() {
+const Title = styled.h1``;
+const Input = styled.input`
+  width:400px;
+  height:30px;
+  font-size: 16px;
+  padding: 10px;
+  boder: 1px solid $000;
+`;
+
+function App (){
+ 
+  const [ price, setPrice ] = useState(0);
+  const [ percent, setPercent ] = useState(10);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <Title>Calculadora de Gorjetas</Title>
+     <p>Qual o valor da Conta ?</p>
+     <Input type="number" value={price} onChange={ (e) => setPrice(parseFloat(e.target.value)) } ></Input>
+     
+     <p>Qual a porcentagem da Gorjeta?</p>
+     <Input type="number" value={percent} onChange={ (e) => setPercent(parseFloat(e.target.value))  } ></Input>
+     <hr/>
+
+    { price > 0 &&
+      <>
+        <p>SubTotal: R$ {price} </p>
+        <p>Gorjeta ( {percent} % ): R$ {(price  * ( percent/100 )).toFixed(2) } </p>
+        <h3>Total: R$ { (price +  ( price  * ( percent/100 ) )).toFixed(2) } </h3>
+
+      </>
+    }
+
+    </>
+  );   
 }
+
+ 
 
 export default App;
